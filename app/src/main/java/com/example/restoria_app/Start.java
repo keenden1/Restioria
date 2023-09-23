89c77c4a425f6219221ae8db33a1231fcfc7b56e
keenden1 <jimmyybautista2000@gmail.com>
2023-09-23 15:26:37 +0300

    First try

---
+++ app/src/main/java/com/example/restoria_app/Start.java
@@ -0,0 +1,20 @@
+package com.example.restoria_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        setContentView(R.layout.activity_start);
+    }
+}
