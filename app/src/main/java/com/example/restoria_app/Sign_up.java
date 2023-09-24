ef59bb1a21afe4ae7b16199d6aae35858862b645
keenden1 <jimmyybautista2000@gmail.com>
2023-09-24 20:57:41 +0300

    New

--- app/src/main/java/com/example/restoria_app/Sign_up.java
+++ app/src/main/java/com/example/restoria_app/Sign_up.java
@@ -2,13 +2,38 @@ package com.example.restoria_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
         setContentView(R.layout.activity_sign_up);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent(Sign_up.this, MainActivity.class);
+        startActivity(intent);
     }
 }
